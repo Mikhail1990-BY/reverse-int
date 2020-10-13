ae8e86014b648359f93aeb7ceb8c3070338bc43b
Mikhail Kaladzianets <68966313+Mikhail1990-BY@users.noreply.github.com>
2020-10-13 22:15:13 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    if (n > 0){
+      return Number(String(n).split('').reverse().join(''));  
+    }
+    else {
+        n = n * (-1);
+        return Number(String(n).split('').reverse().join(''));  
+    }
 }
